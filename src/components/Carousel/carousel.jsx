29c5c37ce0471682e1927145a06a4f77be3c3e84
prajaktahadole <prajaktahadole@gmail.com>
2022-09-17 22:14:34 +0530

    carousel

--- src/components/Carousel/carousel.jsx
+++ src/components/Carousel/carousel.jsx
@@ -1,13 +1,36 @@
-export const Carousel = () => {
-    return (
-        <div>
+import React, { useState } from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+
+const slideImages = [
+
+    "https://images.thrillophilia.com/image/upload/s--uSEJiTF7--/c_fill,f_auto,fl_strip_profile,h_775,q_auto,w_1600/v1/images/photos/000/055/651/original/1550304812_shutterstock_1036392316.jpg.jpg?1550304812",
+    "https://images.thrillophilia.com/image/upload/s--fuJ_KQSr--/c_fill,f_auto,fl_strip_profile,h_775,q_auto,w_1600/v1/images/photos/000/099/928/original/1509539465_Bharatpur-Bird-Sanctuary.jpg.jpg?1509539465",
+    "https://images.thrillophilia.com/image/upload/s--uSEJiTF7--/c_fill,f_auto,fl_strip_profile,h_775,q_auto,w_1600/v1/images/photos/000/055/651/original/1550304812_shutterstock_1036392316.jpg.jpg?1550304812",
+    "https://images.thrillophilia.com/image/upload/s--fuJ_KQSr--/c_fill,f_auto,fl_strip_profile,h_775,q_auto,w_1600/v1/images/photos/000/099/928/original/1509539465_Bharatpur-Bird-Sanctuary.jpg.jpg?1509539465",
   
-
-
-        
+];
 
 
 
-        </div>
-    )
+export const Carousel = () => {
+    const [autoplay, setAutoplay] = useState(true);
+
+  return (
+    <>
+    <div style={{ width: "95%", MaxHeight: "80%" , margin: "auto"}}  >
+        <Slide autoplay={autoplay}>
+            {slideImages.map((item) => (
+                <div className="each-slide" >
+                    <div style={{ width: "100%", height: "auto" }}>
+                        <img src={item} style={{ maxWidth: "100%", maxHeight: "100%" ,  objectFit: "cover" }} />
+                    </div>
+                </div>
+            ))}
+
+        </Slide>
+    </div >
+</>
+   
+  );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,21 @@
 import {Dataa} from "./Dataa";
 import './Home.css';
 import {Carousel} from "../Carousel/carousel";
-import { 
-    Heading } from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
 
 export const Home = () => {
     return (
-       <div >
+       <Box  style={{
+        width: "100%",
+        
+      }}>
+        < Heading as='h1' size='4xl' margin={"15px"} color={"blue.700"}>Welcome to the Wildlife Sanctuary  </Heading>
            <Carousel></Carousel>
-           < Heading as='h1' size='4xl' margin={"15px"} color={"blue.700"}>Welcome to the Wildlife Sanctuary  </Heading>
+           
             <Heading as='h2' size='xl'margin={"15px"}>Get Ready for an Experience You’ll Always Remember!</Heading>
             
             <Heading margin={"35px"}>Gallery</ Heading>
             <Dataa></Dataa>
-       </div>
+       </Box>
     )
 }
